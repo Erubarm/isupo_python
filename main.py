7e0f075bda72e96b58539d05f3e0ac3f40f9f0b1
Erubarm <kirill.komigachev@yandex.ru>
2023-01-17 21:44:07 +0300

    *

---
+++ main.py
@@ -0,0 +1,36 @@
+class Car:
+    """
+    Автомобиль
+    имеет цвет
+    имеет мощность дивгателя
+    имеет максимальный запас бензина в баке
+    имеет расход (например 9.3 литра на 100 км)
+    можно перекрасить
+    можно заправить N литров
+    можно проехать N киллометров
+    можно узнать цвет
+    мощность
+    сколько осталось литров в баке
+    узнать расход
+    """
+    color: str = 'white' # цвет
+    engine_power: int = 500 # мощность двигателя л.с
+    max_tank: int = 75 # максимальный запас бензина
+    consumption: float = 10 # расход на 100км
+
+    def __init__(self, color: str = 'black', engine_power: int = 600, max_tank: int = 9, consumption: float = 8):
+
+        self.color = color
+        self.engine_power = engine_power
+        self.max_tank = max_tank
+        self.consumption = consumption
+
+    def set_color(self):
+        self.color = input()
+
+c = Car('White', 555, 11, 7)
+
+
+
+
+
